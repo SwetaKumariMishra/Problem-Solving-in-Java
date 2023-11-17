f758c681fad836e8d5e77dbfd671d7af2a7b660c
Sweta Kumari Mishra <mishrasweta1001@gmail.com>
2023-11-17 23:12:44 +0530

    Length of last word

---
+++ src/LengthofLastWord.java
@@ -0,0 +1,24 @@
+public class LengthofLastWord {
+
+    public static void main(String[] args) {
+        String s = "Hello Sweta";
+        System.out.println(lengthofLastWord(s));
+    }
+
+    static int lengthofLastWord(String s)
+    {
+        String str = s.trim();
+        int count = 0;
+        for (int i=str.length()-1; i>=0; i--)
+        {
+            if(str.charAt(i) != ' ')
+            {
+                count++;
+            }
+            else {
+                break;
+            }
+        }
+        return count;
+    }
+}
